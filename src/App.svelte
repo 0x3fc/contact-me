dd501f38448b15718ba31f8b3d24efbb22c53f3e
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-09-19 00:09:42 -0700

    Add contact card

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  const name = "CONTACT_NAME";
+  const phone = "CONTACT_PHONE_NUMBER";
+  const email = "CONTACT_EMAIL";
+
+  const copy = text => {
+    const tmp = document.createElement("input");
+    document.body.append(tmp);
+    tmp.value = text;
+    tmp.select();
+    document.execCommand("copy");
+    tmp.remove();
+  };
+</script>
+
+<div class="card">
+  <h2 class="contact-name">{name}</h2>
+  <div class="contact-info">
+    <a href="sms:{phone}" class="contact-link" id="contact-phone">{phone}</a>
+    <button class="clipboard" on:click={() => copy(phone)}>
+      <i class="far fa-clipboard" />
+    </button>
+  </div>
+  <div class="contact-info">
+    <a href="mailto:{email}" class="contact-link">{email}</a>
+    <button class="clipboard" on:click={() => copy(email)}>
+      <i class="far fa-clipboard" />
+    </button>
+  </div>
+</div>
